0ed9beebab9bf0cb00b79c421d8fe68605e2d41f
MeanMachineRex <RexRowan@users.noreply.github.com>
2026-08-14 11:31:05 +0000

    fix indent

--- src/qiskit_stateviz/__init__.py
+++ src/qiskit_stateviz/__init__.py
@@ -1,13 +1,13 @@
 from .bloch import plot_bloch_multivector_interactive
- from .qsphere import plot_qsphere_interactive
-+from .rotation import plot_spin_rotation_interactive
- from .utils import StateVizError
+from .qsphere import plot_qsphere_interactive
+from .rotation import plot_spin_rotation_interactive
+from .utils import StateVizError
 
- __version__ = "0.1.0"
+__version__ = "0.1.0"
 
- __all__ = [
-     "plot_qsphere_interactive",
-     "plot_bloch_multivector_interactive",
-+    "plot_spin_rotation_interactive",
-     "StateVizError",
- ]
+__all__ = [
+    "plot_qsphere_interactive",
+    "plot_bloch_multivector_interactive",
++   "plot_spin_rotation_interactive",
+    "StateVizError",
+]
